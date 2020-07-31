5c678892751f79633b9c6d69f6ccee19da08a6bd
chris0chris <kontakt@kaempfe.ch>
2020-07-31 10:07:44 +0200

    heroku ready

--- league_manager/settings.py
+++ league_manager/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG_VALUE')
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['127.0.0.1', 'dffl5er.herokuapp.com']
 
