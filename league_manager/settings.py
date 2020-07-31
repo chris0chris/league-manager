d90d5aa1d28f333d8642a20749720328c78d3ead
chris0chris <kontakt@kaempfe.ch>
2020-07-31 09:47:51 +0200

    django_heroku installed

--- league_manager/settings.py
+++ league_manager/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -120,3 +122,5 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'league-home'
 LOGIN_URL = 'login'
+
+django_heroku.settings(locals())
