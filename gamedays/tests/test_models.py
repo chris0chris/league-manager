132eb37f9d17bd6172f84164273344e650d3df38
chris0chris <kontakt@kaempfe.ch>
2020-10-30 22:10:29 +0100

    test gameofficials updated

--- gamedays/tests/test_models.py
+++ gamedays/tests/test_models.py
@@ -1,11 +1,15 @@
 from django.test import TestCase
-from gamedays.models import GameOfficial
+
+from gamedays.models import GameOfficial, Gameinfo
+from gamedays.tests.setup_factories.db_setup import DBSetup
 
 
 class TestGameOfficials(TestCase):
 
     def test_officials_are_created(self):
+        DBSetup().g62_status_empty()
+
         assert len(GameOfficial.objects.all()) == 0
-        official = GameOfficial(name='Horst', position='Scorecard')
+        official = GameOfficial(gameinfo=Gameinfo.objects.all().first(), name='Horst', position='Scorecard')
         official.save()
         assert len(GameOfficial.objects.all()) == 1
