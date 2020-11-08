6132df8edc2c5663376b5292ba77d6703482978a
chris0chris <kontakt@kaempfe.ch>
2020-11-08 15:57:24 +0100

    yaml selenium chromedriver added

--- scorecard/tests/test_views.py
+++ scorecard/tests/test_views.py
@@ -1,11 +1,10 @@
 # noinspection PyUnresolvedReferences
 import chromedriver_binary
-
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.urls import reverse
-
+# from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.webdriver import WebDriver
 
 
 class TestScorecardView(StaticLiveServerTestCase):
@@ -13,7 +12,7 @@ class TestScorecardView(StaticLiveServerTestCase):
     def test_javascript(self):
         opts = Options()
         opts.headless = True
-        selenium = WebDriver(options=opts)
+        selenium = webdriver.Chrome(options=opts)
         selenium.implicitly_wait(10)
         selenium.get(f'{self.live_server_url}{reverse("scorecard-test")}')
         assert selenium.find_element_by_class_name('failed').text == '0'
