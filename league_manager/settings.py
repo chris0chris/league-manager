8710459709ebab19c3b9c5589814a61bb4b206b3
LukasPF <Lukas99@live.de>
2020-11-24 18:24:41 +0100

    moved base.html from gamedays to league_manager

--- league_manager/settings.py
+++ league_manager/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'league_manager',
     'rest_framework',
     'scorecard',
     'league_table',
--- teammanager/urls.py
+++ teammanager/urls.py
@@ -5,7 +5,7 @@ from teammanager.views import createteam,showteams,teamdetail,deleteteam,edittea
 
 urlpatterns = [
    path('addteam/', createteam),
-   path('', showteams),
+   path('', showteams,name='teammanager'),
    path('team/<int:team_id>',teamdetail),
    path('deleteteam/<int:team_id>',deleteteam,name='deleteteam'),
    path('editteam/<int:team_id>',editteam,name='editteam'),
