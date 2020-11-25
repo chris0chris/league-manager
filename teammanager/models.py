ad27f283d7306d22a5d9aba0718ac41e82ec41eb
LukasPF <Lukas99@live.de>
2020-11-25 17:57:21 +0100

    added Firstname and Lastname to custom user model in Teammanager

--- teammanager/models.py
+++ teammanager/models.py
@@ -22,7 +22,8 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField('Avatar', upload_to="media/teammanager/avatars", blank=True, null=True)
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
-
+    firstname = models.CharField(max_length=20,null=True)
+    lastname = models.CharField(max_length=20,null=True)
 
 class Permissions(models.Model):
     name = models.CharField(max_length=20)
