62682b05ca60fe07ec9d03a42801b7534a4f2ccc
chris0chris <kontakt@kaempfe.ch>
2021-01-26 10:33:20 +0100

    redux added

--- scorecard/src/components/App.js
+++ scorecard/src/components/App.js
@@ -1,18 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Navbar from "./layout/Navbar";
+
+import { Provider } from "react-redux";
+import store from "../store";
 
 const App = () => {
-  return (
-    <h1>hello zusammen</h1>
-  )
-}
+  return <h1>hello zusammen</h1>;
+};
 
-export default App
-
+export default App;
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+      <Navbar />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('app')
+  document.getElementById("app")
 );
