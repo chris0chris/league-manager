1aaf9ecad06791bfc5d0d3b2973107d05d2eda67
chris0chris <kontakt@kaempfe.ch>
2021-01-27 17:41:25 +0100

    games are fetched and stored in redux

--- scorecard/src/components/scorecard/Gamedays.js
+++ scorecard/src/components/scorecard/Gamedays.js
@@ -1,9 +1,13 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
-import { getGamedays } from "../../actions/gamedays";
+import { getGames } from "../../actions/games";
 
-const Gamedays = ({ gamedays }) => {
+const Gamedays = (props) => {
+  useEffect(() => {
+    console.log("gamedays", props.gamedays);
+    // props.getGamedays();
+  }, [props.gamedays.length]);
   return (
     <>
       <h3>Bitte einen Spieltag auswählen</h3>
@@ -16,12 +20,17 @@ const Gamedays = ({ gamedays }) => {
           </tr>
         </thead>
         <tbody>
-          {gamedays.map((gameday) => (
+          {props.gamedays.map((gameday) => (
             <tr key={gameday.id}>
               <td>{gameday.date}</td>
               <td>{gameday.name}</td>
               <td>
-                <button className="btn btn-success btn-sm">Auswählen </button>
+                <button
+                  onClick={() => props.getGames(gameday.id)}
+                  className="btn btn-success btn-sm"
+                >
+                  Auswählen
+                </button>
               </td>
             </tr>
           ))}
@@ -31,8 +40,12 @@ const Gamedays = ({ gamedays }) => {
   );
 };
 
+const mapStateToProps = (state) => ({
+  gamedays: state.gamedaysReducer.gamedays,
+});
+
 Gamedays.propTypes = {
   gamedays: PropTypes.array.isRequired,
 };
 
-export default Gamedays;
+export default connect(mapStateToProps, { getGames })(Gamedays);
--- scorecard/src/reducers/index.js
+++ scorecard/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import gamedays from "./gamedaysReducer";
 import errors from "./errorsReducer";
 import auth from "./authReducer";
+import games from "./gamesReducer";
 
 export default combineReducers({
   gamedaysReducer: gamedays,
+  gamesReducer: games,
   errorsReducer: errors,
   authReducer: auth,
 });
