72df8563bcd960589713494305d2ca40992c7d7b
chris0chris <kontakt@kaempfe.ch>
2021-01-28 19:55:30 +0100

    simulated event tested

--- scorecard/src/components/scorecard/Gamedays.spec.js
+++ scorecard/src/components/scorecard/Gamedays.spec.js
@@ -9,11 +9,23 @@ const setup = (props = {}) => {
 
 describe("Gamedays Component", () => {
   let component;
+  let mockFunc;
   beforeEach(() => {
-    component = setup({ gamedays: [], onClick: () => {} });
+    mockFunc = jest.fn();
+    component = setup({
+      gamedays: [{ id: 0, date: "date", name: "Test Gameday" }],
+      onClick: mockFunc,
+    });
   });
   it("It should render without erros", () => {
     const wrapper = component.find("h3");
     expect(wrapper.length).toBe(1);
+  });
+
+  it("Should emit callback on click event", () => {
+    const button = component.find("button").at(0);
+    button.simulate("click");
+    const callback = mockFunc.mock.calls.length;
+    expect(callback).toBe(1);
   });
 });
