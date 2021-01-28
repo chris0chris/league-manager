6522c96d990a8d3a67632b874424852006a8a7b8
chris0chris <kontakt@kaempfe.ch>
2021-01-28 11:21:14 +0100

    tests moved to compoment folders

---
+++ scorecard/src/components/scorecard/Gamedays.spec.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Gamedays from "./Gamedays";
+
+const setup = (props = {}) => {
+  const component = shallow(<Gamedays {...props} />);
+  return component;
+};
+
+describe("Gamedays Component", () => {
+  let component;
+  beforeEach(() => {
+    component = setup({ gamedays: null, onClick: null });
+  });
+  fit("It should render without erros", () => {
+    const wrapper = component.find("h3");
+    expect(wrapper.length).toBe(1);
+  });
+});
