1a91492f3feb8a03c42b9e238ea52f70bbaf5d69
chris0chris <kontakt@kaempfe.ch>
2021-02-04 23:17:03 +0100

    ScorecardTable and tests added

---
+++ scorecard/src/components/scorecard/ScorecardTable.js
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const ScorecardTable = ({ entries }) => {
+  return (
+    <div>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>Pkt:</th>
+            <th>6</th>
+            <th>2</th>
+            <th>1</th>
+          </tr>
+        </thead>
+        <tbody>
+          {entries.map((entry) => (
+            <tr key={entry.id}>
+              <td style={{ fontSize: "smaller" }}>{entry.number}</td>
+              <td>{entry.sixPoints}</td>
+              <td>{entry.twoPoints}</td>
+              <td>{entry.onePoint}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+// ScorecardTable.propTypes = {
+
+// }
+
+export default ScorecardTable;
