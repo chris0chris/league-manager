047583a2ba2bba59821361612cf0b25f6c63f783
chris0chris <kontakt@kaempfe.ch>
2021-02-14 11:05:46 +0100

    Finalize component to App added

--- scorecard/src/components/App.js
+++ scorecard/src/components/App.js
@@ -13,12 +13,17 @@ import {store} from '../store';
 import Navbar from './layout/Navbar';
 import Login from './accounts/Login';
 import PrivateRoute from './common/PrivateRoute';
-import {DETAILS_URL, LOGIN_URL, OFFICIALS_URL, ROOT_URL} from './common/urls';
+import {DETAILS_URL,
+  FINALIZE_URL,
+  LOGIN_URL,
+  OFFICIALS_URL,
+  ROOT_URL} from './common/urls';
 import {loadUser} from '../actions/auth';
 
 import SelectGame from './scorecard/SelectGame';
 import Officials from './scorecard/Officials';
 import Details from './scorecard/Details';
+import Finalize from './scorecard/Finalize';
 
 const App = (props) => {
   useEffect(() => {
@@ -32,6 +37,7 @@ const App = (props) => {
             <PrivateRoute exact path={ROOT_URL} component={SelectGame} />
             <PrivateRoute exact path={OFFICIALS_URL} component={Officials} />
             <PrivateRoute exact path={DETAILS_URL} component={Details} />
+            <PrivateRoute exact path={FINALIZE_URL} component={Finalize} />
             <Route exact path={LOGIN_URL} component={Login} />
           </Switch>
         </div>
