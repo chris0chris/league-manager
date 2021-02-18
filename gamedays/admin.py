6f54193b8d4e776a5d627e33ef85b9e4c5b9689d
chris0chris <kontakt@kaempfe.ch>
2021-02-18 19:30:40 +0100

    GameSetup in admin view added

--- gamedays/admin.py
+++ gamedays/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Gameday, Gameinfo, Gameresult, GameOfficial, TeamLog
+from .models import Gameday, Gameinfo, Gameresult, GameOfficial, TeamLog, GameSetup
 
 admin.site.register(Gameday)
 admin.site.register(Gameinfo)
 admin.site.register(Gameresult)
 admin.site.register(GameOfficial)
 admin.site.register(TeamLog)
+admin.site.register(GameSetup)
