7ed8597237eff8dccd43d6fafaeeb370036b5e04
chris0chris <kontakt@kaempfe.ch>
2021-02-21 22:11:09 +0100

    jquery mocked via jest setup

--- scorecard/src/__tests__/setup/jest.setup.js
+++ scorecard/src/__tests__/setup/jest.setup.js
@@ -1,2 +1,8 @@
 import 'regenerator-runtime/runtime';
 import '@testing-library/jest-dom';
+import $ from 'jquery/src/jquery';
+const modalMock = jest.fn();
+jest.mock('jquery/src/jquery', () => jest.fn());
+$.mockImplementation(() => {
+  return {modal: modalMock};
+});
