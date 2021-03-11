c0b97ea09d90972336d8406188ea3b1da036b601
chris0chris <kontakt@kaempfe.ch>
2021-03-11 22:52:27 +0100

    #33 webpack config switched to webpack.config.js and output filename changed

--- liveticker/webpack.config.js
+++ liveticker/webpack.config.js
@@ -1,13 +1,23 @@
+const path = require('path');
+
 module.exports = {
+  // --mode development --watch --entry ./src/index.js --output-path ./static/liveticker/js"
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src/index.js'),
+  watch: true,
+  output: {
+    path: path.resolve(__dirname, 'static/liveticker/js'),
+    filename: 'liveticker.js',
+  },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /\node_modules/,
         use: {
-          loader: "babel-loader"
-        }
-      }
-    ]
-  }
-}
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
