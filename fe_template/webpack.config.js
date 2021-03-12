1ba18c8f332f0fc29e06d92273b31ce96b9b620e
chris0chris <kontakt@kaempfe.ch>
2021-03-12 12:29:09 +0100

    frontend template updated

--- fe_template/webpack.config.js
+++ fe_template/webpack.config.js
@@ -1,22 +1,13 @@
-const path = require('path');
-
 module.exports = {
-  // --mode development --watch --entry ./src/index.js --output-path ./static/liveticker/js"
-  mode: 'development',
-  entry: path.resolve(__dirname, 'src/index.js'),
-  output: {
-    path: path.resolve(__dirname, 'static/liveticker/js'),
-    filename: 'liveticker.js',
-  },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /\node_modules/,
         use: {
-          loader: 'babel-loader',
-        },
-      },
-    ],
-  },
-};
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+}
