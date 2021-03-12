0c9368ed01ff44507acf2b8fdcd785e19c7e746e
chris0chris <kontakt@kaempfe.ch>
2021-03-12 12:28:58 +0100

    #33 default webpack configs, special features via package.json

--- liveticker/webpack.config.js
+++ liveticker/webpack.config.js
@@ -1,10 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  // --mode development --watch --entry ./src/index.js --output-path ./static/liveticker/js"
-  mode: 'development',
   entry: path.resolve(__dirname, 'src/index.js'),
-  watch: true,
   output: {
     path: path.resolve(__dirname, 'static/liveticker/js'),
     filename: 'liveticker.js',
