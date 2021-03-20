fb17e4a3ca04fd83fd4d42dd859ee13fb18db6f6
chris0chris <kontakt@kaempfe.ch>
2021-03-20 11:25:32 +0100

    - deploy liveticker - deploy scorecard - webpack config updated for scorecard

--- scorecard/webpack.config.js
+++ scorecard/webpack.config.js
@@ -1,13 +1,20 @@
+const path = require('path');
+
 module.exports = {
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'static/scorecard/js'),
+    filename: 'scorecard.js',
+  },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /\node_modules/,
         use: {
-          loader: "babel-loader"
-        }
-      }
-    ]
-  }
-}
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
