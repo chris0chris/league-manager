eaae41e24520eb8452242ca13fe4484df82e64f7
chris0chris <kontakt@kaempfe.ch>
2021-04-09 13:33:56 +0200

    deploy script for pythonanywhere

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+echo "activating venv"
+source ../.virtualenvs/venv/bin/activate
+echo "checkout git"
+git pull
+echo "doing django stuff"
+python manage.py migrate
+rm -rf league_manager/static
+python manage.py collectstatic
+echo "reloading webapp"
+touch /var/www/5erdffl_eu_pythonanywhere_com_wsgi.py
+echo "done :-)"
