5108b5c54ae68f3f0e074205313b048d37da940b
chris0chris <kontakt@kaempfe.ch>
2021-04-17 10:26:58 +0200

    debug toolbar added

--- league_manager/urls.py
+++ league_manager/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import debug_toolbar
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -36,3 +37,8 @@ urlpatterns = [
                   # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
                   path('accounts/', include('accounts.urls'))
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns = [
+                      path('__debug__/', include(debug_toolbar.urls)),
+                  ] + urlpatterns
