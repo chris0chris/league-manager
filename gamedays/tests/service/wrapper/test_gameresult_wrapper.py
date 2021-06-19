3f782fe104f4962a11987a7edacfc29d735cfb6c
chris0chris <kontakt@kaempfe.ch>
2021-06-19 12:35:46 +0200

    points against check added

--- gamedays/tests/service/wrapper/test_gameresult_wrapper.py
+++ gamedays/tests/service/wrapper/test_gameresult_wrapper.py
@@ -14,10 +14,14 @@ class TestGameresultWrapper(TestCase):
         gameresult_wrapper.save_away_first_half(9, 12)
         gameresult_wrapper.save_home_second_half(8, 16)
         gameresult_wrapper.save_away_second_half(16, 8)
-        assert Gameresult.objects.get(gameinfo=firstGame, isHome=True).fh == 12
-        assert Gameresult.objects.get(gameinfo=firstGame, isHome=False).fh == 9
-        assert Gameresult.objects.get(gameinfo=firstGame, isHome=True).sh == 8
-        assert Gameresult.objects.get(gameinfo=firstGame, isHome=False).sh == 16
+        first_home = Gameresult.objects.get(gameinfo=firstGame, isHome=True)
+        first_away = Gameresult.objects.get(gameinfo=firstGame, isHome=False)
+        assert first_home.fh == 12
+        assert first_home.sh == 8
+        assert first_home.pa == 25
+        assert first_away.fh == 9
+        assert first_away.sh == 16
+        assert first_away.pa == 20
 
     def test_get_team_names(self):
         DBSetup().g62_status_empty()
