3ca530b2386b44db7c0795746852923d977428e2
chris0chris <kontakt@kaempfe.ch>
2021-06-21 21:53:21 +0200

    language_code changed

--- league_manager/settings/base.py
+++ league_manager/settings/base.py
@@ -102,7 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'de-de'
 
 USE_I18N = True
 TIME_ZONE = 'Europe/Berlin'
