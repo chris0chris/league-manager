d101fba0d6755e877b910579e2d7b4d35f142f9d
chris0chris <kontakt@kaempfe.ch>
2021-06-25 11:44:23 +0200

    easier structure for creating schedule #65

--- gamedays/management/schedule_manager.py
+++ gamedays/management/schedule_manager.py
@@ -5,65 +5,91 @@ import pathlib
 from teammanager.models import Gameday, Gameinfo, Gameresult, Team
 
 
-class ScheduleEntry:
-    def __init__(self, entry):
-        self.entry = entry
-
-    def get_scheduled(self):
-        return self.entry["scheduled"]
-
-    def set_scheduled(self, time):
-        self.entry["scheduled"] = time
-
-    def get_stage(self):
-        return self.entry["stage"]
-
-    def get_standing(self):
-        return self.entry["standing"]
-
-    def get_field(self):
-        return self.entry["field"]
-
-    def get_home(self):
-        return self.entry["home"]
-
-    def get_away(self):
-        return self.entry["away"]
-
-    def get_official(self):
-        return self.entry["official"]
-
-
-class Schedule:
-    def __init__(self, format: str, groups):
-        self.groups = groups
-        self.format = format
-        if int(format.split('_')[0]) != sum(len(group) for group in groups):
-            raise ScheduleTeamMismatchError
-        self.entries = self._get_entries()
-
-    def get_entries(self):
-        return self.entries
-
-    def _get_entries(self):
-        with open(pathlib.Path(__file__).parent / 'schedules/schedule_{0}.json'.format(self.format)) as f:
-            data = json.load(f)
-        entries = []
-        for entry in data:
-            for key in entry:
-                tmp = entry[key].split('_')
-                if len(tmp) > 1:
-                    entry[key] = self.groups[int(tmp[0])][int(tmp[1])]
-            entries.append(ScheduleEntry(entry))
-        return entries
-
-
 class TeamNotExistent(BaseException):
     pass
 
 
 class ScheduleTeamMismatchError(BaseException):
     pass
+
+
+class ScheduleEntry:
+    def __init__(self, schedule_entry: object):
+        self.stage = schedule_entry['stage']
+        self.standing = schedule_entry['standing']
+        self.home = schedule_entry['home']
+        self.away = schedule_entry['away']
+        self.officials = schedule_entry['official']
+
+    def __repr__(self):
+        return f'ScheduleEntry({{stage: "{self.stage}", standing: "{self.standing}", ' \
+               f'home: "{self.home}", away: "{self.away}", official: "{self.officials}"}}'
+
+
+class EmptyScheduleEntry:
+    def __repr__(self):
+        return f'EmptyScheduleEntry()'
+
+
+class FieldSchedule:
+    def __init__(self, field: str, games: []):
+        self.field = field
+        self.games = self._create_schedule_entries(games)
+
+    def _create_schedule_entries(self, games):
+        entries = []
+        for game in games:
+            if len(game) == 0:
+                entries = entries + [EmptyScheduleEntry()]
+            else:
+                entries = entries + [ScheduleEntry(game)]
+        return entries
+
+    def __repr__(self):
+        return f'FieldSchedule(field={self.field}, games={str([schedule_entry for schedule_entry in self.games])})'
+
+
+class Schedule:
+    def __init__(self, gameday_format: str, groups: []):
+        self.format = gameday_format
+        self.groups = groups
+        if not self._format_match_number_of_teams():
+            raise ScheduleTeamMismatchError
+        self.entries = self._get_entries()
+
+    def _get_entries(self):
+        with open(pathlib.Path(__file__).parent / 'schedules/schedule_{0}.json'.format(self.format)) as f:
+            data = json.load(f)
+        entries = []
+        for field_entry in data:
+            field = field_entry['field']
+            games = field_entry['games']
+            entries = entries + [FieldSchedule(field, games)]
+        entries = self._replace_placeholders(entries)
+        return entries
+
+    def _replace_placeholders(self, entries):
+        for field_entry in entries:
+            for game in field_entry.games:
+                if isinstance(game, ScheduleEntry):
+                    game.home = self._replace_placeholder_by_group_entry(game.home)
+                    game.away = self._replace_placeholder_by_group_entry(game.away)
+                    game.officials = self._replace_placeholder_by_group_entry(game.officials)
+        return entries
+
+    def _replace_placeholder_by_group_entry(self, home_placeholder: str):
+        # expected format groupIndex_teamIndex: 0_0
+        tmp = home_placeholder.split('_')
+        # if other format
+        if len(tmp) < 2:
+            return home_placeholder
+        group_index = int(tmp[0])
+        team_index = int(tmp[1])
+        return self.groups[group_index][team_index]
+
+    def _format_match_number_of_teams(self):
+        number_of_teams = int(self.format.split('_')[0])
+        return number_of_teams == sum(len(group) for group in self.groups)
 
 
 class ScheduleCreator:
@@ -73,43 +99,47 @@ class ScheduleCreator:
 
     def create(self):
         Gameinfo.objects.filter(gameday_id=self.gameday.pk).delete()
-        for entry in self.schedule.get_entries():
-            gameinfo = Gameinfo()
-            gameinfo.gameday = self.gameday
-            gameinfo.scheduled = self._calc_scheduled(entry)
-            gameinfo.stage = entry.get_stage()
-            gameinfo.field = entry.get_field()
-            gameinfo.standing = entry.get_standing()
-            if entry.get_official() != '':
-                gameinfo.officials = self._get_team(entry.get_official())
-            gameinfo.save()
+        field_entry: FieldSchedule
+        for field_entry in self.schedule.entries:
+            scheduled = self.gameday.start
+            field = field_entry.field
+            for game in field_entry.games:
+                if isinstance(game, EmptyScheduleEntry):
+                    scheduled = self._calc_next_time_slot(scheduled)
+                    continue
+                self._create_gameinfo_and_gameresult(game, field, scheduled)
+                scheduled = self._calc_next_time_slot(scheduled)
 
-            if entry.get_home() != '' and entry.get_away() != '':
-                home = Gameresult()
-                home.gameinfo = gameinfo
-                home.team = self._get_team(entry.get_home())
-                home.isHome = True
-                home.save()
-                away = Gameresult()
-                away.gameinfo = gameinfo
-                away.team = self._get_team(entry.get_away())
-                away.save()
+    def _create_gameinfo_and_gameresult(self, game, field, scheduled):
+        gameinfo = Gameinfo()
+        gameinfo.gameday = self.gameday
+        gameinfo.scheduled = scheduled
+        gameinfo.stage = game.stage
+        gameinfo.field = field
+        gameinfo.standing = game.standing
+        gameinfo.officials = self._get_team(game.officials)
+        gameinfo.save()
+        self._create_gameresult(game, gameinfo)
 
-    def _get_team(self, teamName):
+    def _get_team(self, team_name):
         try:
-            return Team.objects.get(name=teamName)
+            return Team.objects.get(name=team_name)
         except Team.DoesNotExist:
             Gameinfo.objects.filter(gameday_id=self.gameday.pk).delete()
-            raise TeamNotExistent(teamName)
+            raise TeamNotExistent(team_name)
 
-    def _calc_scheduled(self, entry):
-        if entry.get_scheduled() == 'start':
-            entry.set_scheduled(self.gameday.start)
-            return entry.get_scheduled()
+    def _create_gameresult(self, game: ScheduleEntry, gameinfo: Gameinfo):
+        home = Gameresult()
+        home.gameinfo = gameinfo
+        home.team = self._get_team(game.home)
+        home.isHome = True
+        home.save()
+        away = Gameresult()
+        away.gameinfo = gameinfo
+        away.team = self._get_team(game.away)
+        away.save()
 
-        previous_index = int(entry.get_scheduled())
-        previous_entry = self.schedule.get_entries()[previous_index]
-        time_to_datetime_object = datetime.datetime.combine(datetime.date(1, 1, 1), previous_entry.get_scheduled())
-        scheduled_time = (time_to_datetime_object + datetime.timedelta(minutes=70)).time()
-        entry.set_scheduled(scheduled_time)
-        return entry.get_scheduled()
+    def _calc_next_time_slot(self, scheduled):
+        scheduled_to_datetime_object = datetime.datetime.combine(datetime.date(1, 1, 1), scheduled)
+        new_scheduled_time = (scheduled_to_datetime_object + datetime.timedelta(minutes=70)).time()
+        return new_scheduled_time
