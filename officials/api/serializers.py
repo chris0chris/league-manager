948145bf5b74797f2f14b4d6b84bc59c42c16b46
chris0chris <kontakt@kaempfe.ch>
2021-11-14 15:02:09 +0100

    feat: display all GameOfficials entries
    Due to the dirty game GameOfficials data entries the officials need to have a opportunity
    to see what was tracked.
    Therefore, this dirty hack is implemented which should be deleted when the officials supervised their data.
    
    Refs: #78

--- officials/api/serializers.py
+++ officials/api/serializers.py
@@ -1,9 +1,43 @@
+import re
+
+from rest_framework.fields import CharField, SerializerMethodField
 from rest_framework.serializers import ModelSerializer
 
 from officials.models import Official
+from teammanager.models import GameOfficial
 
 
 class OfficialSerializer(ModelSerializer):
+    team = CharField(source='team.description')
+
     class Meta:
         model = Official
+        fields = '__all__'
+
+
+class GameOfficialAllInfosSerializer(ModelSerializer):
+    info = SerializerMethodField('get_infos')
+    name = SerializerMethodField('obfuscate_name')
+
+    def get_infos(self, object: GameOfficial):
+        home = object.gameinfo.gameresult_set.get(isHome=True).team.name
+        away = object.gameinfo.gameresult_set.get(isHome=False).team.name
+        return {
+            'team': object.gameinfo.officials.name,
+            'gameday': object.gameinfo.gameday.name,
+            'date': object.gameinfo.gameday.date.strftime('%d.%m.%Y'),
+            'vs': home + ' vs ' + away,
+            'standing': object.gameinfo.standing,
+        }
+
+    def obfuscate_name(self, object: GameOfficial):
+        return re.sub('\B[a-zäöüÄÖÜßé]', '*', object.name)
+
+    def _obfuscate_name(self, name: str):
+        first_letter = name[0]
+        all_other_letters = name[1:]
+        return "".join((first_letter, all_other_letters.replace(all_other_letters, "****")))
+
+    class Meta:
+        model = GameOfficial
         fields = '__all__'
