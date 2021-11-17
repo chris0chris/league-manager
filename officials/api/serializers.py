43591b3e082cda4ddccd1a36f833326cd9a6a132
chris0chris <kontakt@kaempfe.ch>
2021-11-17 20:16:04 +0100

    fix: display names when authenticated and link to teams
    Refs: #78

--- officials/api/serializers.py
+++ officials/api/serializers.py
@@ -19,11 +19,17 @@ class GameOfficialAllInfosSerializer(ModelSerializer):
     info = SerializerMethodField('get_infos')
     name = SerializerMethodField('obfuscate_name')
 
+    def __init__(self, display_names_for_team=None, is_staff=False, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.display_names_for_team = display_names_for_team
+        self.is_staff = is_staff
+
     def get_infos(self, object: GameOfficial):
         home = object.gameinfo.gameresult_set.get(isHome=True).team.name
         away = object.gameinfo.gameresult_set.get(isHome=False).team.name
         return {
             'team': object.gameinfo.officials.name,
+            'team_id': object.gameinfo.officials.pk,
             'gameday': object.gameinfo.gameday.name,
             'date': object.gameinfo.gameday.date.strftime('%d.%m.%Y'),
             'vs': home + ' vs ' + away,
@@ -31,12 +37,9 @@ class GameOfficialAllInfosSerializer(ModelSerializer):
         }
 
     def obfuscate_name(self, object: GameOfficial):
+        if self.display_names_for_team == object.gameinfo.officials.name or self.is_staff:
+            return object.name
         return re.sub('\B[a-zäöüÄÖÜßé]', '*', object.name)
-
-    def _obfuscate_name(self, name: str):
-        first_letter = name[0]
-        all_other_letters = name[1:]
-        return "".join((first_letter, all_other_letters.replace(all_other_letters, "****")))
 
     class Meta:
         model = GameOfficial
--- officials/urls.py
+++ officials/urls.py
@@ -7,6 +7,8 @@ OFFICIALS_LIST_FOR_TEAM_AND_YEAR = 'view-officials-list-for-team-and-year'
 OFFICIALS_LIST_FOR_ALL_TEAMS = 'view-officials-list-for-all-teams'
 OFFICIALS_LIST_FOR_ALL_TEAMS_AND_YEAR = 'view-officials-list-for-all-teams-and-year'
 OFFICIALS_GAME_OFFICIALS_APPEARANCE = 'view-officials-game-officials-appearance'
+OFFICIALS_GAME_OFFICIALS_APPEARANCE_FOR_TEAM = 'view-officials-game-officials-appearance-for-team'
+OFFICIALS_GAME_OFFICIALS_APPEARANCE_FOR_TEAM_AND_YEAR = 'view-officials-game-officials-appearance-for-team-and-year'
 
 urlpatterns = [
     path('team/<int:pk>/list', OfficialsTeamListView.as_view(), name=OFFICIALS_LIST_FOR_TEAM),
@@ -14,4 +16,9 @@ urlpatterns = [
     path('team/all/list', AllOfficialsListView.as_view(), name=OFFICIALS_LIST_FOR_ALL_TEAMS),
     path('team/all/list/<int:year>', AllOfficialsListView.as_view(), name=OFFICIALS_LIST_FOR_ALL_TEAMS_AND_YEAR),
     path('einsaetze', GameOfficialListView.as_view(), name=OFFICIALS_GAME_OFFICIALS_APPEARANCE),
+    path('team/all/gamelist', GameOfficialListView.as_view(), name=OFFICIALS_GAME_OFFICIALS_APPEARANCE),
+    path('team/all/gamelist/<int:year>', GameOfficialListView.as_view(), name=OFFICIALS_GAME_OFFICIALS_APPEARANCE),
+    path('team/<int:pk>/gamelist', GameOfficialListView.as_view(), name=OFFICIALS_GAME_OFFICIALS_APPEARANCE_FOR_TEAM),
+    path('team/<int:pk>/gamelist/<int:year>', GameOfficialListView.as_view(),
+         name=OFFICIALS_GAME_OFFICIALS_APPEARANCE_FOR_TEAM_AND_YEAR),
 ]
