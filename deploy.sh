c8b3ef0f8e377486f212b6fcf1325ed60be541f1
chris0chris <kontakt@kaempfe.ch>
2022-09-19 18:02:56 +0200

    doc: document how to mirror database

--- deploy.sh
+++ deploy.sh
@@ -12,3 +12,18 @@ python manage.py collectstatic
 echo "reloading webapp"
 touch /var/www/5erdffl_eu_pythonanywhere_com_wsgi.py
 echo "done :-)"
+
+# reset python environment
+# rm -rf .virtualenvs/
+# mkvirtualenv venv --python=/usr/bin/python3.8
+
+# EXPORT database
+#mysqldump -u 5erdffl -h 5erdffl.mysql.eu.pythonanywhere-services.com --set-gtid-purged=OFF --no-tablespaces '5erdffl$league_manager'  > db-backup.sql
+# IMPORT remote database
+#mysql -u 5erdffl -h 5erdffl.mysql.eu.pythonanywhere-services.com '5erdffl$league_manager'  < db-backup.sql
+
+# IMPORT database local
+# mysql -u root -p 5erdffl$league_manager < db-backup.sql
+
+# EXPORT database local
+#mysqldump -u root -p --set-gtid-purged=OFF --no-tablespaces 5erdffl$league_manager  > db-backup.sql
