f3315d451103d94c18cd6ecb8f29886ae167b26e
chris0chris <kontakt@kaempfe.ch>
2023-03-22 20:22:59 +0100

    test: skip test for circleci

--- officials/tests/service/moodle/test_moodle_api.py
+++ officials/tests/service/moodle/test_moodle_api.py
@@ -1,13 +1,19 @@
+import os
+
+import pytest
+
 from officials.service.moodle.moodle_api import MoodleApi, ApiUserInfo, ApiCourses, ApiParticipants
 
 
 class TestMoodleApi:
 
+    @pytest.mark.skipif(os.environ.get('CIRCLECI'), reason='CIRCLECI will reveal secrets')
     def test_get_all_courses(self):
         moodle_api = MoodleApi()
         all_courses: ApiCourses = moodle_api.get_courses()
         assert len(all_courses.get_all()) > 1
 
+    @pytest.mark.skipif(os.environ.get('CIRCLECI'), reason='CIRCLECI will reveal secrets')
     def test_get_all_participants_for_course(self):
         moodle_api = MoodleApi()
         participants: ApiParticipants = moodle_api.get_participants_for_course(5)
@@ -16,6 +22,7 @@ class TestMoodleApi:
         assert all_participants[3].get_result() == 13
         assert all_participants[3].get_user_id() == 11
 
+    @pytest.mark.skipif(os.environ.get('CIRCLECI'), reason='CIRCLECI will reveal secrets')
     def test_get_user_info(self):
         moodle_api = MoodleApi()
         user_info: ApiUserInfo = moodle_api.get_user_info(11)
