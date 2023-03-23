5cdddfe63317bc6ddd72b9d2acc5c9b21d1cbd40
chris0chris <kontakt@kaempfe.ch>
2023-03-23 11:54:46 +0100

    fix: make liveticker React 18 ready

--- liveticker/src/components/LivetickerApp.js
+++ liveticker/src/components/LivetickerApp.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {store} from '../store';
 import 'regenerator-runtime/runtime';
+
 import Liveticker from './liveticker/Liveticker';
 
 
 const LivetickerApp = (props) => {
   return (
-    <div className='container mt-2'>
-      <Liveticker />
-    </div>
+    <Provider store={store}>
+      <div className='container mt-2'>
+        <Liveticker />
+      </div>
+    </Provider>
   );
 };
 
 export default LivetickerApp;
-
-ReactDOM.render(
-    <React.StrictMode>
-      <Provider store={store}>
-        <LivetickerApp />
-      </Provider>
-    </React.StrictMode>,
-    document.getElementById('liveticker-app'),
-);
--- liveticker/src/index.js
+++ liveticker/src/index.js
@@ -1,2 +1,14 @@
 // eslint-disable-next-line no-unused-vars
 import LivetickerApp from './components/LivetickerApp';
+
+import React, {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+
+const rootElement = document.getElementById('liveticker-app');
+const root = createRoot(rootElement);
+
+root.render(
+    <StrictMode>
+      <LivetickerApp />
+    </StrictMode>,
+);
