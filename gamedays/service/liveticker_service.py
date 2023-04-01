8821f40e9db2136a2fcbda838e176926c7270f9a
chris0chris <kontakt@kaempfe.ch>
2023-04-01 11:56:09 +0200

    fix: display all ticks

--- gamedays/service/liveticker_service.py
+++ gamedays/service/liveticker_service.py
@@ -97,7 +97,7 @@ class Liveticker(object):
 
 class LivetickerService(object):
     def __init__(self, gameday_id=None, game_ids_with_all_ticks=(), league=[]):
-        self.game_ids_with_all_ticks = game_ids_with_all_ticks
+        self.game_ids_with_all_ticks = list(map(int, game_ids_with_all_ticks))
         if not league:
             league = League.objects.all()
         else:
