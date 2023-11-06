20c869cae550994f85edde5b0eb0299d3f565545
Nik <justniklas1@gmail.com>
2023-11-06 12:34:25 +0100

    Initial commit Teamoverview FE

--- passcheck/src/components/Headerdata.tsx
+++ passcheck/src/components/Headerdata.tsx
@@ -4,9 +4,11 @@ function Headerdata() {
   return (
     <>
       <h1>Passcheck von {teamDataJson.teamname}</h1>
+    </>
+    /*
       <div>Feld: {teamDataJson.field}</div>
       <div>Kickoff: {teamDataJson.kickoff}</div>
-    </>
+    </> */
   );
 }
 
