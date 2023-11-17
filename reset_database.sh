5cf467c9c1c6d7a69044346ac1e2a7559775b2e6
chris0chris <kontakt@kaempfe.ch>
2023-11-17 16:01:43 +0100

    fix: remove cd #116

--- reset_database.sh
+++ reset_database.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/bash
 echo "truncating all tables"
 mysql -u demodffl -h demodffl.mysql.eu.pythonanywhere-services.com -Nse 'show tables' 'demodffl$default' | while read table; do echo "SET FOREIGN_KEY_CHECKS = 0;truncate table $table;"; done | mysql -u demodffl -h demodffl.mysql.eu.pythonanywhere-services.com 'demodffl$default'
-echo "change dir to league_manager"
-cd league_manager
 echo "activating venv"
 source ../.virtualenvs/venv/bin/activate
 echo "importing demo_database.json"
