71c723d6b6ed971f0d74b100afecc24729feb059
chris0chris <kontakt@kaempfe.ch>
2024-01-13 13:25:31 +0100

    feat: add passcheck vs scorecard selection #1122

---
+++ scorecard/src/components/scorecard/SelectPasscheckOrScorecard.js
@@ -0,0 +1,36 @@
+/* eslint-disable max-len */
+import React from 'react';
+import {Navigate} from 'react-router-dom';
+import {SELECT_GAME_URL} from '../common/urls';
+import {useState} from 'react';
+
+const SelectPasscheckOrScorecard = () => {
+  const [redirectToScorecard, setRedirectToScorecard] = useState(false);
+
+  const redirectToPasscheck = () => {
+    window.location.href = '/passcheck';
+  };
+  if (redirectToScorecard) {
+    console.log('navigating to ', SELECT_GAME_URL);
+    return <Navigate to={SELECT_GAME_URL} />;
+  }
+  return (
+    <div className='select-container d-flex align-items-center justify-content-center'>
+      <button
+        className='btn btn-primary btn-lg m-2'
+        onClick={redirectToPasscheck}
+      >
+        Passcheck
+      </button>
+      <button
+        className='btn btn-secondary btn-lg m-2'
+        onClick={() => setRedirectToScorecard(true)}
+      >
+        Scorecard
+      </button>
+    </div>
+  );
+};
+SelectPasscheckOrScorecard.propTypes = {};
+
+export default SelectPasscheckOrScorecard;
