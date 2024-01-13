dec51bb4e701c94676765fbc326f4efcfc5433e3
chris0chris <kontakt@kaempfe.ch>
2024-01-13 13:25:31 +0100

    feat: add passcheck vs scorecard selection #1122

--- scorecard/src/components/App.js
+++ scorecard/src/components/App.js
@@ -1,10 +1,6 @@
 import React, {useEffect} from 'react';
 import {Provider} from 'react-redux';
-import {
-  HashRouter as Router,
-  Route,
-  Routes,
-} from 'react-router-dom';
+import {HashRouter as Router, Route, Routes} from 'react-router-dom';
 
 import 'regenerator-runtime/runtime';
 
@@ -12,10 +8,13 @@ import {store} from '../store';
 import Navbar from './layout/Navbar';
 import Login from './accounts/Login';
 import PrivateRoute from './common/PrivateRoute';
-import {DETAILS_URL,
+import {
+  DETAILS_URL,
   FINALIZE_URL,
   LOGIN_URL,
-  OFFICIALS_URL} from './common/urls';
+  OFFICIALS_URL,
+  SELECT_GAME_URL,
+} from './common/urls';
 import {loadUser} from '../actions/auth';
 import {getPenalties} from '../actions/config';
 
@@ -24,6 +23,7 @@ import Officials from './scorecard/Officials';
 import Details from './scorecard/Details';
 import Finalize from './scorecard/Finalize';
 import MessageToaster from './scorecard/MessageToaster';
+import SelectPasscheckOrScorecard from './scorecard/SelectPasscheckOrScorecard';
 
 const App = (props) => {
   useEffect(() => {
@@ -35,20 +35,39 @@ const App = (props) => {
   return (
     <Provider store={store}>
       <Router>
-        <div className="container mt-2">
-          <div className="row">
+        <div className='container mt-2'>
+          <div className='row'>
             <Routes>
               <Route exact path={LOGIN_URL} element={<Login />} />
-              <Route exact path={OFFICIALS_URL}
-                element={<PrivateRoute component={Officials} />} />
-              <Route exact path={DETAILS_URL}
-                element={<PrivateRoute component={Details} />} />
-              <Route exact path={FINALIZE_URL}
-                element={<PrivateRoute component={Finalize} />} />
-              <Route exact path="/*"
-                element={<PrivateRoute component={SelectGame} />} />
               <Route
-                path="*"
+                exact
+                path={OFFICIALS_URL}
+                element={<PrivateRoute component={Officials} />}
+              />
+              <Route
+                exact
+                path={DETAILS_URL}
+                element={<PrivateRoute component={Details} />}
+              />
+              <Route
+                exact
+                path={FINALIZE_URL}
+                element={<PrivateRoute component={Finalize} />}
+              />
+              <Route
+                exact
+                path={SELECT_GAME_URL}
+                element={<PrivateRoute component={SelectGame} />}
+              />
+              <Route
+                exact
+                path='/*'
+                element={
+                  <PrivateRoute component={SelectPasscheckOrScorecard} />
+                }
+              />
+              <Route
+                path='*'
                 element={
                   <main style={{padding: '1rem'}}>
                     <p>There is nothing here!</p>
@@ -57,19 +76,16 @@ const App = (props) => {
               />
             </Routes>
           </div>
-          <div className="row">
+          <div className='row'>
             <MessageToaster />
           </div>
         </div>
-        <div className="container my-1 py-4">
+        <div className='container my-1 py-4'>
           <Navbar />
         </div>
       </Router>
     </Provider>
-
   );
 };
 
 export default App;
-
-
--- scorecard/src/components/common/urls.js
+++ scorecard/src/components/common/urls.js
@@ -3,5 +3,5 @@ export const OFFICIALS_URL = '/officials';
 export const DETAILS_URL = '/details';
 export const LOGIN_URL = '/login';
 export const _URL = '/';
-export const FINALIZE_URL = '/finalize'
-;
+export const FINALIZE_URL = '/finalize';
+export const SELECT_GAME_URL = '/select-game';
