29f3ae2eb71516de6bdfed8b8f720da7bfb1171a
chris0chris <kontakt@kaempfe.ch>
2024-01-19 14:14:11 +0100

    feat: add overview for players counting games #123

---
+++ gamedays/templatetags/custom_filters.py
@@ -0,0 +1,8 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter(name='get_item')
+def get_item(dictionary, key):
+    return dictionary.get(f'{key}', None)
--- passcheck/service/passcheck_service.py
+++ passcheck/service/passcheck_service.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 from urllib.parse import unquote
 
+from django.db.models import Count, Q
 from knox.models import AuthToken
 
 from gamedays.models import Team, Gameinfo
-from passcheck.api.serializers import PasscheckGamesListSerializer, PasscheckSerializer
+from passcheck.api.serializers import PasscheckGamesListSerializer, PasscheckSerializer, RosterSerializer
 from passcheck.models import Playerlist
 
 
@@ -33,13 +34,27 @@ class PasscheckService:
         if officials_id:
             date = datetime.today()
             date = '2023-08-05'
-            return PasscheckGamesListSerializer(Gameinfo.objects.filter(officials_id=officials_id, gameday__date=date), many=True).data
+            return PasscheckGamesListSerializer(Gameinfo.objects.filter(officials_id=officials_id, gameday__date=date),
+                                                many=True).data
 
     def get_passcheck_data(self, token):
         team_name = self.get_team_username(token_key=token)
         return {
             'officialsTeamName': self.get_team_name(team_username=team_name),
             'games': self.get_officiating_games(officials_id=self.get_officials(teamname=team_name)),
+        }
+
+    def get_roster(self, team: Team):
+        all_leagues = Playerlist.objects.filter(team=team).exclude(gamedays__league__name=None).distinct().values(
+            'gamedays__league', 'gamedays__league__name')
+        league_annotations = {
+            f'{league["gamedays__league"]}': Count('gamedays__league',
+                                                   filter=Q(gamedays__league=league['gamedays__league'])) for
+            league in all_leagues}
+        roster = Playerlist.objects.filter(team=team).annotate(**league_annotations).values()
+        return {
+            'all_leagues': list(all_leagues),
+            'roster': RosterSerializer(instance=roster, context={'all_leagues': list(all_leagues)}, many=True).data
         }
 
 
