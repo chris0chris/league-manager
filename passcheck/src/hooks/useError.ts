94c880d7a45bcd8ab5576ca83539abc08d8599d5
chris0chris <kontakt@kaempfe.ch>
2024-02-06 15:14:40 +0100

    feat: add minimumStrength validation #136

--- passcheck/src/hooks/useError.ts
+++ passcheck/src/hooks/useError.ts
@@ -3,8 +3,6 @@ import ErrorContext from '../context/ErrorContext';
 
 const useError = () => {
   const {error, setError} = useContext(ErrorContext);
-  console.log('useError HOOK -> error:', error);
-  // setError(new Error('useError()'));
   return {error, setError};
 };
 
