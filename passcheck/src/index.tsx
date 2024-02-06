3c89337663bec54ec7a841d1e245467d87684b2e
chris0chris <kontakt@kaempfe.ch>
2024-02-06 17:52:00 +0100

    feat: display additional team in accordion #134

--- passcheck/src/index.tsx
+++ passcheck/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import reportWebVitals from './reportWebVitals';
 import './passcheck.css';
 
