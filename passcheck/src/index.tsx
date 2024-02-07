ace4e232c28b6a171c9472890736e4b5073d1d82
chris0chris <kontakt@kaempfe.ch>
2024-02-07 11:49:51 +0100

    refactor: easier build mechanics and index.html #141

--- passcheck/src/index.tsx
+++ passcheck/src/index.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import reportWebVitals from './reportWebVitals';
-import './passcheck.css';
+// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('passcheck') as HTMLElement
@@ -23,4 +20,4 @@ if (debug) {
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
