74a16476e2324446e348d4d7e06a9586d7ecb6b9
chris0chris <kontakt@kaempfe.ch>
2024-02-08 20:05:08 +0100

    feat: show gamedays per player #129

--- passcheck/urls.py
+++ passcheck/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path
 
-# import views
-from .views import PasscheckListView, PlayerlistCreateView, PlayerlistView, PasscheckView
+from .views import PasscheckPlayerGamesList, PlayerlistCreateView, PlayerlistView, PasscheckView
 
+PASSCHECK_APP = 'passcheck-app-view'
+
+PASSCHECK_PLAYER_GAMES_LIST = 'passcheck-player-games-list'
 PASSCHECK_ROSTER_CREATE = 'passcheck-roster-create'
+PASSCHECK_ROSTER_LIST = 'passcheck-roster-list'
 
-# mapping urls to different views
 urlpatterns = [
-    path('', PasscheckView.as_view(), name='passcheck-view'),
-    path('list/', PasscheckListView.as_view(), name='passcheck-list'),
+    path('', PasscheckView.as_view(), name=PASSCHECK_APP),
+    path('player/<int:id>/games/list/<int:year>', PasscheckPlayerGamesList.as_view(), name=PASSCHECK_PLAYER_GAMES_LIST),
+    path('player/<int:id>/games/list', PasscheckPlayerGamesList.as_view(), name=PASSCHECK_PLAYER_GAMES_LIST),
     path('roster/create', PlayerlistCreateView.as_view(), name=PASSCHECK_ROSTER_CREATE),
-    path('roster/<int:team>/list', PlayerlistView.as_view(), name=PASSCHECK_ROSTER_CREATE),
+    path('roster/<int:team>/list', PlayerlistView.as_view(), name=PASSCHECK_ROSTER_LIST),
 ]
