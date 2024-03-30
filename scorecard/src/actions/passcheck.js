d80efd6496d43116c343c49ef3517745c46f1197
chris0chris <chris@flag-coaching.info>
2024-03-30 10:39:09 +0100

    fix: endpoint for passcheck game status

--- scorecard/src/actions/passcheck.js
+++ scorecard/src/actions/passcheck.js
@@ -2,5 +2,5 @@ import {GET_PASSCHECK_STATUS} from './types';
 import {apiGet} from './utils/api';
 
 export const getPasscheckStatus = () => {
-  return apiGet('api/passcheck/games/status', GET_PASSCHECK_STATUS);
+  return apiGet(`${window.location.origin}/api/passcheck/games/status`, GET_PASSCHECK_STATUS);
 };
