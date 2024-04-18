eb4893e347b1ff5b9995efe6b604e23fddb805f6
chris0chris <chris@flag-coaching.info>
2024-04-18 21:31:58 +0200

    feat: display officials on gameday_detail.html #154

--- gamedays/views.py
+++ gamedays/views.py
@@ -33,16 +33,30 @@ class GamedayDetailView(DetailView):
             'escape': False,
             'table_id': 'schedule',
         }
+        qualify_table = gs.get_qualify_table().to_html(**render_configs)
         if 'officials' in settings.INSTALLED_APPS:
+            show_official_names = False
+            if self.request.user.is_staff:
+                show_official_names = True
+            elif self.request.user.username:
+                show_official_names = self.request.user.username in qualify_table
             from officials.service.signup_service import OfficialSignupService
-            officials = OfficialSignupService.get_signed_up_officials(pk, True)
+            officials = OfficialSignupService.get_signed_up_officials(pk, show_official_names)
+            from officials.urls import OFFICIALS_PROFILE_LICENSE
+            url_pattern_official = OFFICIALS_PROFILE_LICENSE
+            from officials.urls import OFFICIALS_SIGN_UP_LIST
+            url_pattern_official_signup = OFFICIALS_SIGN_UP_LIST
         else:
             officials = []
+            url_pattern_official = ''
+            url_pattern_official_signup = ''
         context['info'] = {
             'schedule': gs.get_schedule().to_html(**render_configs),
-            'qualify_table': gs.get_qualify_table().to_html(**render_configs),
+            'qualify_table': qualify_table,
             'final_table': gs.get_final_table().to_html(**render_configs),
-            'officials': officials
+            'officials': officials,
+            'url_pattern_official': url_pattern_official,
+            'url_pattern_official_signup': url_pattern_official_signup,
         }
         return context
 
