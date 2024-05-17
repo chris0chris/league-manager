04a941403f46cd2833b3b256f066c1315b6ce56a
chris0chris <chris@flag-coaching.info>
2024-05-17 21:51:04 +0200

    refactor: add deprecated scorecard for reference #159

--- scorecard/urls.py
+++ scorecard/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from scorecard.views import ScorecardView
+from scorecard.views import ScorecardView, ScorecardViewDeprecated
 
 urlpatterns = [
     path('', ScorecardView.as_view(), name='scorecard-home'),
+    path('deprecated', ScorecardViewDeprecated.as_view(), name='scorecard-home'),
 ]
--- scorecard/views.py
+++ scorecard/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class ScorecardView(TemplateView):
     template_name = 'scorecard/index.html'
+
+
+class ScorecardViewDeprecated(TemplateView):
+    template_name = 'scorecard/index_deprecated.html'
