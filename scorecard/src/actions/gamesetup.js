5add6db3811e2072ffbde9a40ef28e3a0e07ed8f
chris0chris <chris@flag-coaching.info>
2024-05-18 21:21:21 +0200

    refactor: autoformat

--- scorecard/src/actions/gamesetup.js
+++ scorecard/src/actions/gamesetup.js
@@ -5,37 +5,31 @@ import {
   GAME_SETUP_OFFICIALS_SUCCESS,
   GAME_SETUP_OFFICIALS_FAIL,
   GET_GAME_SETUP,
-} from './types';
-import {apiGet, apiPut} from './utils/api';
+} from "./types";
+import { apiGet, apiPut } from "./utils/api";
 
 export const getGameSetup = (gameId) => {
-  return apiGet(
-      `/api/game/${gameId}/setup`,
-      GET_GAME_SETUP,
-  );
+  return apiGet(`/api/game/${gameId}/setup`, GET_GAME_SETUP);
 };
 
 export const saveGameSetup = (gameId, gameSetup) => {
   return apiPut(
-      `/api/game/${gameId}/setup`,
-      gameSetup,
-      GAME_SETUP_SUCCESS,
-      GAME_SETUP_FAIL,
+    `/api/game/${gameId}/setup`,
+    gameSetup,
+    GAME_SETUP_SUCCESS,
+    GAME_SETUP_FAIL,
   );
 };
 
 export const getOfficials = (gameId) => {
-  return apiGet(
-      `/api/game/${gameId}/officials`,
-      GET_GAME_OFFICIALS,
-  );
+  return apiGet(`/api/game/${gameId}/officials`, GET_GAME_OFFICIALS);
 };
 
 export const saveOfficials = (gameId, officials) => {
   return apiPut(
-      `/api/game/${gameId}/officials`,
-      officials,
-      GAME_SETUP_OFFICIALS_SUCCESS,
-      GAME_SETUP_OFFICIALS_FAIL,
+    `/api/game/${gameId}/officials`,
+    officials,
+    GAME_SETUP_OFFICIALS_SUCCESS,
+    GAME_SETUP_OFFICIALS_FAIL,
   );
 };
