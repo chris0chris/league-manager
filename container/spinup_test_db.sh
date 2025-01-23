ab506985d4e571e4fba1dc82d273d68e91c485f6
dachrisch <christian.daehn@gmail.com>
2025-01-23 20:50:53 +0100

    test db scripts

--- container/spinup_test_db.sh
+++ container/spinup_test_db.sh
@@ -1,2 +1,3 @@
-docker run -d --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=user -e MYSQL_DATABASE=test_db mariadb:lts
+docker rm -f mysql
+docker run -d --name mysql -p 3306:3306 -v test_user.sql:/docker-entrypoint-initdb.d/init.sql -e MYSQL_ROOT_PASSWORD=user -e MYSQL_DATABASE=test_db mariadb:lts
 
