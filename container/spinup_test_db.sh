694758f58a4dc53fa00a3308eb216c06024c20b1
dachrisch <christian.daehn@gmail.com>
2025-01-23 21:10:05 +0100

    test db scripts

--- container/spinup_test_db.sh
+++ container/spinup_test_db.sh
@@ -1,3 +1,11 @@
-docker rm -f mysql
-docker run -d --name mysql -p 3306:3306 -v $(pwd)/test_user.sql:/docker-entrypoint-initdb.d/init.sql -e MYSQL_ROOT_PASSWORD=user -e MYSQL_DATABASE=test_db mariadb:lts
-
+#!/bin/zsh
+echo "starting test container"
+pushd ~/dev/infrastructure/container/scripts/
+./setup_test_container.sh
+popd
+sleep 10
+echo "starting test db"
+ssh servyy-test.lxd "docker rm -f mysql && docker run -d --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=user -e MYSQL_DATABASE=test_db mariadb:lts"
+sleep 20
+echo "setup test db"
+ssh servyy-test.lxd "docker exec -i mysql mariadb -puser -uroot "< test_user.sql
