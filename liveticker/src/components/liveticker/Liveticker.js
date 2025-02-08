c0f6db040f4c640630fbe0d2100cbdbdf288478a
dachrisch <christian.daehn@gmail.com>
2025-02-08 13:43:27 +0100

    Remove max-len eslint disable comments from files

--- liveticker/src/components/liveticker/Liveticker.js
+++ liveticker/src/components/liveticker/Liveticker.js
@@ -1,4 +1,3 @@
-/* eslint-disable max-len */
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
--- liveticker/src/index.js
+++ liveticker/src/index.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line no-unused-vars
 import LivetickerApp from './components/LivetickerApp';
 
 import React, {StrictMode} from 'react';
