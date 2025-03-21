b569b6ba9a4e20395849c5b170605b0a3d6db67a
dachrisch <christian.daehn@gmail.com>
2025-03-22 00:36:25 +0100

    Update ALLOWED_HOSTS and add localhost as allowed host

--- league_manager/settings/prod.py
+++ league_manager/settings/prod.py
@@ -1,5 +1,5 @@
 from .base import *
 
 DEBUG = False
-ALLOWED_HOSTS = ['5erdffl.eu.pythonanywhere.com', '127.0.0.1', 'leaguesphere.app']
+ALLOWED_HOSTS = ['5erdffl.eu.pythonanywhere.com', '127.0.0.1', 'leaguesphere.app', 'localhost']
 CSRF_TRUSTED_ORIGINS = ['https://leaguesphere.app', 'https://www.leaguesphere.app']
