d5a47e58aad789ca3809818ef2ff4c7a6b50e6a9
dachrisch <christian.daehn@gmail.com>
2025-03-21 23:18:41 +0100

    Update prod settings for hostnames and origins

--- league_manager/settings/prod.py
+++ league_manager/settings/prod.py
@@ -1,3 +1,3 @@
 DEBUG = False
-ALLOWED_HOSTS = ['5erdffl.eu.pythonanywhere.com', '127.0.0.1', 'lm.lehel.xyz']
-CSRF_TRUSTED_ORIGINS = ['https://lm.lehel.xyz']
+ALLOWED_HOSTS = ['5erdffl.eu.pythonanywhere.com', '127.0.0.1', 'leaguesphere.app']
+CSRF_TRUSTED_ORIGINS = ['https://leaguesphere.app', 'https://www.leaguesphere.app']
