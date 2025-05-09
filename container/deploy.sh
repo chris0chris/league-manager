6d98d0feda05b3d9e134d220bc3b9702de8e163a
dachrisch <christian.daehn@gmail.com>
2025-05-09 18:25:17 +0200

    refactor(deploy): simplify deploy script with version bump

--- container/deploy.sh
+++ container/deploy.sh
@@ -1,45 +1,32 @@
 #!/bin/bash
 
-# Define the branches
-MASTER_BRANCH="master"
-DEPLOY_BRANCH="deploy/latest"
-REMOTE="origin"
+show_help() {
+    echo "Usage: $0 [major|minor|patch]"
+    echo
+    echo "Options:"
+    echo "  major     Bump the major version"
+    echo "  minor     Bump the minor version"
+    echo "  patch     Bump the patch version"
+    echo "  -h, --help  Show this help message and exit"
+}
 
-# Fetch latest changes from remote
-echo "Fetching latest changes from [$REMOTE]..."
-git fetch $REMOTE
+# No arguments provided
+if [ $# -eq 0 ]; then
+    echo "Error: No option provided."
+    show_help
+    exit 1
+fi
 
-# Switch to master and pull latest changes
-echo "Checking out [$MASTER_BRANCH]..."
-git checkout $MASTER_BRANCH || exit 1
-
-echo "Pulling latest changes into [$MASTER_BRANCH]..."
-git pull $REMOTE $MASTER_BRANCH || exit 1
-
-# Update deploy/latest branch
-echo "Checking out [$DEPLOY_BRANCH]..."
-git checkout $DEPLOY_BRANCH || exit 1
-
-echo "Pulling latest changes into [$DEPLOY_BRANCH]..."
-git pull || exit 1
-
-echo "Rebase [$MASTER_BRANCH] onto [$DEPLOY_BRANCH]..."
-git rebase $MASTER_BRANCH || exit 1
-
-LAST_COMMIT_MSG=$(git log -1 --pretty=%B master)
-GIT_COMMITTER_NAME="$(git show -s --format=%cn $MASTER_BRANCH)"
-
-git commit --amend -m "deploy [$LAST_COMMIT_MSG] from [$GIT_COMMITTER_NAME]" || exit 1
-
-echo "Pulling after rebase..."
-git pull || exit 1
-
-echo "Pushing updates to [$REMOTE/$DEPLOY_BRANCH]..."
-git push $REMOTE $DEPLOY_BRANCH || exit 1
-
-# Prune local branches that no longer exist on remote
-echo "Cleaning up local branches that no longer exist on [$REMOTE]..."
-git remote prune $REMOTE || exit 1
-
-git checkout $MASTER_BRANCH
-echo "Update process complete!"
+case "$1" in
+    major|minor|patch)
+        (cd ..;bump2version "$1") && git push && git push --tags
+        ;;
+    -h|--help)
+        show_help
+        ;;
+    *)
+        echo "Error: Invalid option '$1'"
+        show_help
+        exit 1
+        ;;
+esac
