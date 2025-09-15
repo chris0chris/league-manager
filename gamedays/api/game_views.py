9e2351f17f9a3a24cef13993edd3fef89222c49f
chris0chris <kaempfech@gmail.com>
2025-09-15 21:28:34 +0200

    refactor: remove print statements

--- gamedays/api/game_views.py
+++ gamedays/api/game_views.py
@@ -127,7 +127,6 @@ class GamesToWhistleAPIView(APIView):
         gs = GamedayService.create(kwargs['pk'])
         games_to_whistle = gs.get_games_to_whistle(kwargs.get('team'))
         games_to_whistle = games_to_whistle.to_json(orient='records')
-        print(json.dumps(json.loads(games_to_whistle), indent=2))
         return Response(json.loads(games_to_whistle, object_pairs_hook=OrderedDict))
 
 
