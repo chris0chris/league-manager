0ef0efa45658c5f33865555d009841e23a792753
chris0chris <kaempfech@gmail.com>
2025-10-28 20:47:44 +0100

    fix: add caching for dal teams #557

--- league_manager/dal/views.py
+++ league_manager/dal/views.py
@@ -1,16 +1,19 @@
 from dal import autocomplete
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 
 from gamedays.models import Team
 
 
+@method_decorator(cache_page(60 * 60 * 24), name='dispatch')
 class TeamAutocompleteView(autocomplete.Select2QuerySetView):
+    paginate_by = 500
     def get_queryset(self):
         qs = Team.objects.exclude(location='dummy').order_by('description')
-
         if self.q:
-            qs = qs.filter(description__icontains=self.q).exclude(location='dummy').order_by('description')
-
+            qs = qs.filter(description__icontains=self.q)
         return qs
 
     def get_result_label(self, item):
         return item.description
+
