a6e43cc06133418a0ac3347bf799ebf6d28c7e97
chris0chris <kaempfech@gmail.com>
2026-01-04 14:16:39 +0100

    refactor: make LeagueSeasonConfigAdmin more verbose #597

--- league_table/admin.py
+++ league_table/admin.py
@@ -29,3 +29,14 @@ class LeagueRulesetAdmin(admin.ModelAdmin):
 @admin.register(LeagueSeasonConfig)
 class LeagueSeasonConfigAdmin(admin.ModelAdmin):
     form = LeagueSeasonConfigForm
+
+    list_display = ('league', 'season', 'ruleset', 'leagues', 'excluded_gamedays')
+    ordering = ("league__name","-season__name")
+
+    def leagues(self, obj):
+        return ", ".join([league.name for league in obj.leagues_for_league_points.all().order_by('name')])
+    leagues.short_description = "Ligen"
+
+    def excluded_gamedays(self, obj):
+        return ", ".join([gameday.name for gameday in obj.exclude_gamedays.all().order_by('date')])
+    excluded_gamedays.short_description = "Ausgeschlossene Spieltage"
