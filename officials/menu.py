f31b488c684ed68d850a66c87cf882a30f16ea83
chris0chris <kaempfech@gmail.com>
2026-04-08 21:35:18 +0200

    feat: add officials url to menu if user is_superuser #821

--- officials/menu.py
+++ officials/menu.py
@@ -2,6 +2,8 @@ from league_manager.base_menu import BaseMenu, MenuItem, MenuDivider
 from officials.urls import (
     OFFICIALS_LIST_FOR_ALL_TEAMS,
     OFFICIALS_GAME_OFFICIALS_APPEARANCE,
+    OFFICIALS_GAMEOFFICIAL_INTERNAL_CREATE,
+    OFFICIALS_LICENSE_CHECK,
 )
 
 
@@ -10,7 +12,7 @@ class OfficialsMenu(BaseMenu):
         return "Offizielle"
 
     def get_menu_items(self, request):
-        return [
+        items = [
             MenuItem.create(
                 name="Übersicht",
                 url=OFFICIALS_LIST_FOR_ALL_TEAMS,
@@ -19,10 +21,37 @@ class OfficialsMenu(BaseMenu):
                 name="Alle Einsätze",
                 url=OFFICIALS_GAME_OFFICIALS_APPEARANCE,
             ),
-            MenuDivider.create(),
-            MenuItem.create(
-                name="Einsätze melden",
-                url="https://offd.de/offd/moodle/mod/page/view.php?id=315",
-                is_static=True,
-            ),
         ]
+        if request.user.is_superuser:
+            items.append(
+                MenuItem.create(
+                    name="Offizielle Internal Eintrag",
+                    url=OFFICIALS_GAMEOFFICIAL_INTERNAL_CREATE,
+                )
+            )
+            items.append(
+                MenuItem.create(
+                    name="Moodle Report",
+                    url="/officials/moodle-report/?ids=",
+                    is_static=True,
+                )
+            )
+            items.append(
+                MenuItem.create(
+                    name="Moodle Lizenz check",
+                    url=OFFICIALS_LICENSE_CHECK,
+                    url_kwargs={"course_id": "15"},
+                )
+            )
+        items.extend(
+            [
+                MenuDivider.create(),
+                MenuItem.create(
+                    name="Einsätze melden",
+                    url="https://offd.de/offd/moodle/mod/page/view.php?id=315",
+                    is_static=True,
+                ),
+            ]
+        )
+
+        return items
