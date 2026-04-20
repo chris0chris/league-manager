ee1164798bbd277ec456f9c21612669e8ec3e729
dachrisch <christian.daehn@gmail.com>
2026-04-20 20:02:14 +0200

    Revert "fix(static): collect static files in container entrypoints"
    This reverts commit bb8460ec16a86d7b2f575dcc9439857653fc2163.

--- container/entrypoint.demo.sh
+++ container/entrypoint.demo.sh
@@ -39,9 +39,6 @@ init_database() {
     python manage.py migrate --noinput
     log "Migrations completed"
 
-    python manage.py collectstatic --noinput
-    log "Static files collected"
-
     python manage.py seed_demo_data
     log "Demo data seeded"
 
